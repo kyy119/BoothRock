977029e3067d5f916a94887715c15244a349e3d6
89s-web-diary <kangyg21c@gmail.com>
2024-01-10 10:25:03 +0900

    Refactor : "축제 리스트페이지 기본순/인기순 구현"
    - 기존 list3 xml 수정
    - fstv_list.js 파일 기본순 / 인기순 버튼에 따라 작동방식 나뉘게 변경

--- src/main/webapp/resources/js/fstv_list.js
+++ src/main/webapp/resources/js/fstv_list.js
@@ -1,21 +1,35 @@
 jQuery(document).ready(function(){
 
 	let page = 1;
-
+	let list_url = "fstv_list";
 	$("#def-btn").click(function(){
+		list_url = "fstv_list";
 		$("#def-btn").addClass("bold-text");
 		$("#pop-btn").removeClass("bold-text");
-	});
-	
-    $("#pop-btn").click(function(){						// 인기순 버튼을 누를때
-    	$("#pop-btn").addClass("bold-text");			// 인기순 버튼 볼드처리
-        $("#def-btn").removeClass("bold-text");			// 기본순 버튼 볼드처리 해제
-        $.ajax({										// 동시에 ajax
-        	url : "fstv_jjim",							// fstv_jjim 어노테이션으로 이동
+		$.ajax({
+        	url : list_url,								// 기본순(fstv_list)으로 지정된 url 어노테이션으로 이동
         	data : {
         	page : "1"									// 페이지값 1지정 후 idx계산해서 첫페이지 출력
         	},
         	success : function(list) {
+        		$('.fstv-list').empty();				// 기존 fstv-list에 있던 내용들 지우고
+        		$('.fstv-list').html(list);				// 기본순 정렬을 불러온다
+        		page = 1;								// 인기순으로 늘어나있던 페이지를 다시 1로 돌려주고 스크롤할때 새로 불러와준다
+        	}
+        })
+	});
+	
+    $("#pop-btn").click(function(){						// 인기순 버튼을 누를때
+    	list_url = "fstv_jjim";							// 불러오는 url을 jjim url으로 변경
+    	$("#pop-btn").addClass("bold-text");
+        $("#def-btn").removeClass("bold-text");
+        $.ajax({										// 동시에 ajax
+        	url : list_url,								// 인기순(fstv_jjim)으로 지정된 url 어노테이션으로 이동
+        	data : {
+        	page : "1"									// 페이지값 1지정 후 idx계산해서 첫페이지 출력
+        	},
+        	success : function(list) {
+        		console.log(list_url);
         		$('.fstv-list').empty();				// 기존 fstv-list에 있던 내용들 지우고
         		$('.fstv-list').html(list);				// 인기순 정렬을 불러온다
         		page = 1;								// 기본순으로 늘어나있던 페이지를 다시 1로 돌려주고 스크롤할때 새로 불러와준다
@@ -29,7 +43,7 @@ jQuery(document).ready(function(){
 	
 	// 축제 리스트 첫 페이지를 위한 ajax, idx 0~9까지의 결과를 가져온다
 	$.ajax({
-		url : "fstv_list",
+		url : list_url,
 		data : {
 		page : "1"
 		},
@@ -46,9 +60,10 @@ jQuery(document).ready(function(){
 	        // 스크롤이 페이지 하단에 도달하고 로딩 중이 아닐 때
 	        loading = true; 								// 로딩 상태로 변경
 	        page++;											// 페이지 1추가
+	        console.log(page);
 	
 	        $.ajax({										// AJAX를 사용하여 새로운 리스트를 서버에서 가져옴
-	            url: 'fstv_list',
+	            url: list_url,
 	            data : {
 	            	page : page								// 스크롤할때마다 추가된 page를 controller에 전달
 	            },
