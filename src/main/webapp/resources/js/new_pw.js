74479be57f3a55cb8e7a8f4500b5c8abf659a4b4
kyy119 <97082148+kyy119@users.noreply.github.com>
2024-01-16 10:10:01 +0900

    Revert "Fix : "부스상세페이지 review tab 구현오류 수정""

--- src/main/webapp/resources/js/new_pw.js
+++ src/main/webapp/resources/js/new_pw.js
@@ -1,45 +1,50 @@
 jQuery(document).ready(function() {
-	$(document).ready(function() {
-		var submitButtons = document.getElementsByClassName("pw-edit");
-		submitButtons[0].disabled = true;
-		 let passwordIsValid = false;
-		 let passwordSameIsValid = false;
-		 
-		 $('#user_password').on('blur', function() {
-			var messageElement = document.getElementById('password_box');
-			var passwordInput = document.getElementById('user_password');
-	        // 입력 값이 변경되었을 때 실행되는 함수
-		 	passwordIsValid = validatePassword($(this).val());
+	let editButtons = $('.pw-edit');
+	editButtons[0].disabled = true;
+	let passwordIsValid = false;
+	let passwordSameIsValid = false;
+	console.log(editButtons[0].disabled);	 
+	$('#user_password').on('blur', function() {
+	  // 입력 값이 변경되었을 때 실행되는 함수
+		passwordIsValid = validatePassword($(this).val());
 				 
-			if (passwordIsValid && $(this).val() != '') {
-				passwordIsValid = true;
-				messageElement.textContent = "사용할수있는 비밀번호입니다!" 
-			} else {
-				messageElement.textContent = "사용할수없는 비밀번호입니다!"
-			}
-			messageElement.style.color = passwordIsValid ? 'green' : 'red';
-       		passwordInput.style.borderColor = passwordIsValid ? 'green' : 'red'
-			console.log('패스워드1 : ' + passwordIsValid); // 삭제예정 
-     	 }); // 입력 값이 변경되었을 때 실행되는 함수
+		if (passwordIsValid && $(this).val() != '') {
+			passwordIsValid = true;
+			passwordMessage.textContent = "사용할수있는 비밀번호입니다!"
+			passwordMessage.style.color = "green";
+			passwordMessage.style.display = "block"; 
+		} else {
+			editButtons[0].disabled = true;
+			passwordMessage.textContent = "비밀번호 조건을 만족해주세요!(8자이상,대소문자,숫자,특수문자(@$!%*?&))";
+            passwordMessage.style.color = "red";
+            passwordMessage.style.display = "block";
+		}
+		console.log('패스워드1 : ' + passwordIsValid); // 삭제예정 
+     }); // 입력 값이 변경되었을 때 실행되는 함수
 		 
-		 $('#user_password_check').on('blur', function() {
-		 	var messageElement = document.getElementById('password_box2');
-		 	var passwordInput = document.getElementById('user_password_check');
-			if($('#user_password').val() == $(this).val() && $(this).val() != ''){
-				passwordSameIsValid = true;
-				messageElement.textContent = "비밀번호가 일치합니다!" 
-				if(passwordIsValid == true){
-					submitButtons[0].disabled = false;
-				}
-			}else{
-				passwordSameIsValid = false;
-				messageElement.textContent = "비밀번호가 일치하지않습니다!"
-			}
-			 messageElement.style.color = passwordSameIsValid ? 'green' : 'red';
-       		 passwordInput.style.borderColor = passwordSameIsValid ? 'green' : 'red'
-			console.log('패스워드2 : ' + passwordSameIsValid);// 삭제예정 
-		});// 비밀번호와 비밀번호 확인 로직
-	}); // document.ready
+	$('#user_password_check').on('blur', function() {
+		if(passwordIsValid == true && $('#user_password').val() == $(this).val() && $(this).val() != ''){
+			passwordSameIsValid = true;
+			passwordMessage2.style.color = "green";
+			passwordMessage2.textContent = "비밀번호가 일치합니다!" 
+            passwordMessage2.style.display = "block";
+			editButtons[0].disabled = false;
+		}else{
+			editButtons[0].disabled = true;
+			passwordSameIsValid = false;
+			passwordMessage2.style.color = "red";
+			passwordMessage2.textContent = "비밀번호가 일치하지않습니다!"
+            passwordMessage2.style.display = "block";
+		}
+		console.log('패스워드2 : ' + passwordSameIsValid);// 삭제예정 
+	});// 비밀번호와 비밀번호 확인 로직
+		
+    $('.pw-edit').click(function(){
+    	if(editButtons[0].disabled){
+    		alert("회원가입 조건을 만족하지않습니다!");
+    		return false;
+    	}
+    })
 }); // 제이쿼리 전체
 
 function validatePassword(password) { // 비밀번호 유효성 검사 함수
