d8956f72a0816639f2a616f0030e1698400eff12
Yumi Kim <yumii2307@naver.com>
2024-01-24 09:30:18 +0900

    Feat: "관리자 문의 게시판 구현"

--- src/main/java/com/multi/FM/manager/ManagerDAO.java
+++ src/main/java/com/multi/FM/manager/ManagerDAO.java
@@ -13,112 +13,135 @@ import com.multi.FM.users.UsersVO;
 @Repository
 public class ManagerDAO {
 
-	@Autowired
-	SqlSessionTemplate my;
-	
-    public List<UsersVO> user_list(PagingVO pagingVO) {
-        return my.selectList("manager.user_list", pagingVO);
-    }
+  @Autowired
+  SqlSessionTemplate my;
 
-    public List<UsersVO> user_search(PagingVO pagingVO) {
-        return my.selectList("manager.user_search", pagingVO);
-    }
-    
-    public List<UsersVO> ban_user_list(PagingVO pagingVO) {
-        return my.selectList("manager.ban_user_list", pagingVO);
-    }
-    
-    public List<UsersVO> ban_user_search(PagingVO pagingVO) {
-        return my.selectList("manager.ban_user_search", pagingVO);
-    }
+  public List<UsersVO> user_list(PagingVO pagingVO) {
+    return my.selectList("manager.user_list", pagingVO);
+  }
 
-    public UsersVO user_detail(UsersVO usersVO) {
-        return my.selectOne("manager.user_detail", usersVO);
-    }
-    
-    public void update_user(UsersVO usersVO) {
-        my.update("manager.update_user", usersVO);
-    }
-    
-    public void update_admin(UsersVO usersVO) {
-        my.update("manager.update_admin", usersVO);
-    }
-    
-    public void update_seller(UsersVO usersVO) {
-        my.update("manager.update_seller", usersVO);
-    }
-    
-    public List<BoothVO> booth_list(PagingVO pagingVO) {
-        return my.selectList("manager.booth_list", pagingVO);
-    }
+  public List<UsersVO> user_search(PagingVO pagingVO) {
+    return my.selectList("manager.user_search", pagingVO);
+  }
 
-    public List<BoothVO> booth_search(PagingVO pagingVO) {
-        return my.selectList("manager.booth_search", pagingVO);
-    }
-    
-    public List<BoothVO> ban_booth_list(PagingVO pagingVO) {
-      return my.selectList("manager.ban_booth_list", pagingVO);
-    }
-    
-    public List<BoothVO> ban_booth_search(PagingVO pagingVO) {
-      return my.selectList("manager.ban_booth_search", pagingVO);
-    }
+  public List<UsersVO> ban_user_list(PagingVO pagingVO) {
+    return my.selectList("manager.ban_user_list", pagingVO);
+  }
 
-    public void update_booth(BoothVO boothVO) {
-      my.update("manager.update_booth", boothVO);
-    }
-    
-	public List<ReportVO> report_list(PagingVO pagingVO) throws Exception {
-		return my.selectList("manager.report_list", pagingVO);
-	}
-    
-	public List<ReportVO> report_search(PagingVO pagingVO) throws Exception {
-	    return my.selectList("manager.report_search", pagingVO);
-	}
-    
-    public ReportVO report_detail(ReportVO reportVO) {
-      return my.selectOne("manager.report_detail", reportVO);
-    }
+  public List<UsersVO> ban_user_search(PagingVO pagingVO) {
+    return my.selectList("manager.ban_user_search", pagingVO);
+  }
 
-    public List<AskVO> ask_list(PagingVO pagingVO) {
-      return my.selectList("manager.ask_list", pagingVO);
-    }
+  public UsersVO user_detail(UsersVO usersVO) {
+    return my.selectOne("manager.user_detail", usersVO);
+  }
 
-    public AskVO ask_detail(AskVO askVO) {
-      return my.selectOne("manager.ask_detail", askVO);
-    }
+  public void update_user(UsersVO usersVO) {
+    my.update("manager.update_user", usersVO);
+  }
 
-    public List<AskVO> ask_search(String type, String word) {
-      Map<String, String> parameters = new HashMap<>();
-      parameters.put("type", type);
-      parameters.put("word", word);
-      
-      return my.selectList("manager.ask_search", parameters);
-    }
-    
-    public int total_count(PagingVO pagingVO) {
-      return my.selectOne("manager.total_count", pagingVO);
-    }
-    
-    public int report_count(PagingVO pagingVO) {
-      return my.selectOne("manager.report_count", pagingVO);
-    }
+  public List<UsersVO> common_user_list(Map<String, Integer> map) {
+    return my.selectList("manager.common_user_list", map);
+  }
 
-    public int user_count(PagingVO pagingVO) {
-      return my.selectOne("manager.user_count", pagingVO);
-    }
+  public void update_admin(UsersVO usersVO) {
+    my.update("manager.update_admin", usersVO);
+  }
 
-    public int ban_user_count(PagingVO pagingVO) {
-      return my.selectOne("manager.ban_user_count", pagingVO);
-    }
-    
-    public int booth_count(PagingVO pagingVO) {
-      return my.selectOne("manager.booth_count", pagingVO);
-    }
-    
-    public int ban_booth_count(PagingVO pagingVO) {
-      return my.selectOne("manager.ban_booth_count", pagingVO);
-    }
+  public void update_seller(UsersVO usersVO) {
+    my.update("manager.update_seller", usersVO);
+  }
 
+  public List<BoothVO> booth_list(PagingVO pagingVO) {
+    return my.selectList("manager.booth_list", pagingVO);
+  }
+
+  public List<BoothVO> booth_search(PagingVO pagingVO) {
+    return my.selectList("manager.booth_search", pagingVO);
+  }
+
+  public List<BoothVO> ban_booth_list(PagingVO pagingVO) {
+    return my.selectList("manager.ban_booth_list", pagingVO);
+  }
+
+  public List<BoothVO> ban_booth_search(PagingVO pagingVO) {
+    return my.selectList("manager.ban_booth_search", pagingVO);
+  }
+
+  public void update_booth(BoothVO boothVO) {
+    my.update("manager.update_booth", boothVO);
+  }
+
+  public List<ReportVO> report_list(PagingVO pagingVO) {
+    return my.selectList("manager.report_list", pagingVO);
+  }
+
+  public List<ReportVO> report_search(PagingVO pagingVO) {
+    return my.selectList("manager.report_search", pagingVO);
+  }
+
+  public ReportVO report_detail(ReportVO reportVO) {
+    return my.selectOne("manager.report_detail", reportVO);
+  }
+
+  public void update_report(ReportVO reportVO) {
+    my.update("manager.update_report", reportVO);
+  }
+
+  public List<AskVO> ask_list(PagingVO pagingVO) {
+    return my.selectList("manager.ask_list", pagingVO);
+  }
+  
+  public List<AskVO> ask_search(PagingVO pagingVO) {
+    return my.selectList("manager.ask_search", pagingVO);
+  }
+
+  public AskVO ask_detail(AskVO askVO) {
+    return my.selectOne("manager.ask_detail", askVO);
+  }
+
+  public void update_ask(AskVO askVO) {
+    my.update("manager.update_ask", askVO);
+  }
+  
+  public int total_count(PagingVO pagingVO) {
+    return my.selectOne("manager.total_count", pagingVO);
+  }
+
+  public int user_count(PagingVO pagingVO) {
+    return my.selectOne("manager.user_count", pagingVO);
+  }
+
+  public int ban_user_count(PagingVO pagingVO) {
+    return my.selectOne("manager.ban_user_count", pagingVO);
+  }
+
+  public int booth_count(PagingVO pagingVO) {
+    return my.selectOne("manager.booth_count", pagingVO);
+  }
+
+  public int ban_booth_count(PagingVO pagingVO) {
+    return my.selectOne("manager.ban_booth_count", pagingVO);
+  }
+
+  public int report_count(PagingVO pagingVO) {
+    return my.selectOne("manager.report_count", pagingVO);
+  }
+  
+  public int ask_count(PagingVO pagingVO) {
+    return my.selectOne("manager.ask_count", pagingVO);
+  }
+
+  public int total_user_count(Map<String, Integer> map) {
+    return my.selectOne("manager.total_user_count", map);
+  }
+
+  public List<UsersVO> common_search(Map<String, Integer> map) {
+    return my.selectList("manager.common_search", map);
+  }
+
+  public int search_user_count(Map<String, Integer> map) {
+    return my.selectOne("manager.search_user_count", map);
+  }
 
 }
--- src/main/webapp/WEB-INF/views/manager/report_detail.jsp
+++ src/main/webapp/WEB-INF/views/manager/report_detail.jsp
@@ -1,5 +1,5 @@
 <%@page import="com.multi.FM.manager.ReportVO"%>
-<%@page import="java.util.List"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <% ReportVO vo = (ReportVO)request.getAttribute("report_detail"); %>
@@ -11,6 +11,25 @@
     <title>모든 축제의 부스를 담다 - 부스락</title>
     <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/report_detail.css" type="text/css">
     <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/jquery-3.7.1.js"></script>
+	<script>
+    	$(document).ready(function() {
+	        function update_report(action, message) {
+	            $.ajax({
+	                type: "POST",
+	                url: action,
+	                data: { report_no: '<%= vo.getReport_no() %>' },
+	                success: function(data) {
+	                    alert(message);
+	                    window.location.href = 'report_list';
+	                }
+	            });
+	        }
+
+	        $(".report_lie").on("click", function() {
+	        	update_report("update_report", "허위 신고 변경이 완료되었습니다.");
+	        });
+	    });
+    </script>
 </head>
 <body>
     <%@ include file="../../../manager/admin_header.jsp" %>
@@ -20,21 +39,27 @@
 			<h3 class="back">
 				<a href="report_list">Report List <i class="fa-solid fa-chevron-right"></i></a>
 			</h3>
-			<div class="report-booth-fstv">
-				<a href="booth_detail?booth_no=<%= vo.getBooth_no() %>" class="booth-title"><%= vo.getBooth_name() %></a>
-				<span> / <a href="fstv_detail?fstv_no=<%= vo.getFstv_no() %>"><%= vo.getFstv_title() %></a></span>
-			</div>
-			<div class="report-detail">
-				<h2><%= vo.getReport_title() %></h2>
-				<div class="report-info"><%= vo.getUser_id() %> | <%= vo.getReport_date() %></div>
-				<p class="report-content"><%= vo.getReport_comment() %></p>
-			<hr>
-		   	</div>
-        	<div class="btn">
-        		<button>판매자 상세 페이지</button>
-        		<button>허위 신고 등록</button>
-        		<button>부스 신고 누적</button>
-        	</div>
+			<c:if test="${not empty report_detail}">
+				<div class="report-booth-fstv">
+					<a href="../booth/booth_detail?booth_no=${report_detail.booth_no}" class="booth-title">${report_detail.booth_name}</a>
+					<span> / <a href="../fstv/fstv_detail?fstv_no=${report_detail.fstv_no}">${report_detail.fstv_title}</a></span>
+				</div>
+				<div class="report-detail">
+					<h2>${report_detail.report_title}</h2>
+					<div class="report-info">${report_detail.user_id} | ${report_detail.report_date}</div>
+					<p class="report-content">${report_detail.report_comment}</p>
+				<hr>
+			   	</div>
+	        	<div class="btn">
+	        		<button onclick="location.href='user_detail?user_id=${report_detail.seller_id}'">판매자 상세 페이지</button>
+	        		<c:if test="${report_detail.report_lie == 0}">
+	        			<button class="report_lie">허위 신고 등록</button>
+	        		</c:if>
+	        		<c:if test="${report_detail.report_lie == 1}">
+	        			<button class="report_lie">허위 신고 등록 해제</button>
+	        		</c:if>
+	        	</div>
+        	</c:if>
 		</div>
     </div>
     
